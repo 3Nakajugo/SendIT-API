c783bec9e48df1e9d0c4799ee3cf7870d55abd3a
3Nakajugo <edinahmargarita@gmail.com>
2021-04-29 12:49:47 +0300

    feat(login): login user
    - add flask-jwt-extended
    - add login endpoint
    
    [Delivers #177301308]

--- app/auth/auth_controller.py
+++ app/auth/auth_controller.py
@@ -1,6 +1,13 @@
 from flask import jsonify, request
 from .auth_model import users, User
 from ..utils.validator import AuthValidator
+from flask_jwt_extended import create_access_token, JWTManager
+from app import app
+import os
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
+jwt = JWTManager(app)
 
 
 class UserController:
@@ -36,4 +43,22 @@ class UserController:
 
         user = User(email=email, password=password).to_dict()
         users.append(user)
-        return jsonify(user), 201
+        return jsonify({"user": user}), 201
+
+    def login_user(self):
+        request_data = request.get_json()
+        email = request_data['email']
+        password = request_data['password']
+
+        for user in users:
+            if user["password"] == password and user["email"] == email:
+                token = create_access_token(identity=email)
+                return jsonify({
+                    "message": "Login successful",
+                    "status": 200,
+                    "token": token
+                }), 200
+        return jsonify({
+            "message": "User with credentials doesnot exist",
+            "status": 404,
+        }), 404
